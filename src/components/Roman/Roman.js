e88595b32d3757d15c79479bf15141197deac921
woonandez <matthew.allen.fernandez@gmail.com>
2020-05-23 09:17:21 -0700

    Add padding and flex throughout the app

--- src/components/Roman/Roman.js
+++ src/components/Roman/Roman.js
@@ -4,11 +4,11 @@ import './Roman.css';
 function Roman(props) {
   return (
     <div className="romanContainer">
-      <h2>{props.romanNumeralConversionValue}</h2>
       <form>
         Enter Roman Numerals Here:
         <input type="text" name="roman"/>
       </form>
+      <h4>{props.romanNumeralConversionValue}</h4>
     </div>
   );
 }
