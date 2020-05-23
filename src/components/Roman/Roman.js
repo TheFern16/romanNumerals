9779c25206a47e71ccae3fa70b6321dff8412207
woonandez <matthew.allen.fernandez@gmail.com>
2020-05-23 09:14:05 -0700

    Add flex and roman numerals form

--- src/components/Roman/Roman.js
+++ src/components/Roman/Roman.js
@@ -4,8 +4,11 @@ import './Roman.css';
 function Roman(props) {
   return (
     <div className="romanContainer">
-      <h2>Hello World</h2>
       <h2>{props.romanNumeralConversionValue}</h2>
+      <form>
+        Enter Roman Numerals Here:
+        <input type="text" name="roman"/>
+      </form>
     </div>
   );
 }
