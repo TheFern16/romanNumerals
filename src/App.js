a259bff44a033ebaa464eb4024f80319b28fc870
woonandez <matthew.allen.fernandez@gmail.com>
2020-05-23 09:05:11 -0700

    Add roman numeral comp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import logo from './clockface_roman01.jpg';
+import logo from './assets/clockface_roman01.jpg';
+import Roman from './components/Roman/Roman.js';
 import './App.css';
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
           Learn about Roman Numerals
         </a>
       </header>
+      <Roman />
     </div>
   );
 }
---
+++ src/components/Roman/Roman.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Roman.css';
+
+function Roman() {
+  return (
+    <div className="">
+      <h2>Hello World</h2>
+    </div>
+  );
+}
+
+export default Roman;
