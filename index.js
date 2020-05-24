f13f853da89b2c426a1c6eca018a4ecbb5673c28
woonandez <matthew.allen.fernandez@gmail.com>
2020-05-24 14:37:49 -0700

    Add server code

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 1337;
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/build'));
+
+app.get('', (req, res) => {
+  res.sendFile(__dirname + '/build/index.html');
+});
+
+app.listen(port, () => {
+  console.log(`|2 U ${port}`);
+});
