cc70a6a39859ff9bc9b83fa17c31e9c85ec2c550
woonandez <matthew.allen.fernandez@gmail.com>
2020-05-24 07:52:49 -0700

    Adjust app test link for roman numerals

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,12 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders roman numeral image', () => {
+
+})
+
+test('renders learn about roman numerals link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/learn about roman numerals/i);
   expect(linkElement).toBeInTheDocument();
 });
