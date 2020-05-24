39d70c3b27ffc4c1df8998474784fed39baf6c6a
woonandez <matthew.allen.fernandez@gmail.com>
2020-05-24 08:06:54 -0700

    Create test for logo render

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import logo from './assets/clockface_roman01.jpg';
 import App from './App';
 
-test('renders roman numeral image', () => {
+test('app component renders roman numeral image', () => {
+  const { getByAltText } = render(<App />);
+  const imageElement = getByAltText(/logo/i);
+  expect(imageElement).toBeInTheDocument();
+});
 
-})
-
-test('renders learn about roman numerals link', () => {
+test('app component renders learn about roman numerals link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn about roman numerals/i);
   expect(linkElement).toBeInTheDocument();
