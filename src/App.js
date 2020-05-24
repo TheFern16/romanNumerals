1d40e663a89904ea5eae32c3772f36b8efdf8c9a
woonandez <matthew.allen.fernandez@gmail.com>
2020-05-24 10:26:03 -0700

    Code cleanup

--- src/App.js
+++ src/App.js
@@ -70,12 +70,7 @@ class App extends React.Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>Roman Numeral Converter</p>
-          <a
-            className="App-link"
-            href="https://en.wikipedia.org/wiki/Roman_numerals"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="App-link" href="https://en.wikipedia.org/wiki/Roman_numerals" target="_blank">
             Learn about Roman Numerals
           </a>
           <div className="buttonContainer">
